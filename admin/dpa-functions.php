e76507d43a062c121c39fa44caae6bcb2f0b07dd
Paul Gibbs <paul@byotos.com>
2012-02-10 18:06:37 +0000

    Update wporg query to cache contributors. Convert raw contributors into Gravatar URLs.

--- admin/dpa-functions.php
+++ admin/dpa-functions.php
@@ -38,10 +38,12 @@ function dpa_filter_sample_permalink( $post_link, $post, $leavename, $sample ) {
  * Returned array consists of nested arrays and objects, one for each
  * supported plugin. Structure is:
  *
+ * ['plugin_ID']->contributors *        Array of key/value pairs for each contributor (User name gravatar URL)
  * ['plugin_ID']->name         *        Plugin name
  * ['plugin_ID']->description  *        Plugin description
- * ['plugin_ID']->wporg_url             wporg/extends URL page for this plugin
  * ['plugin_ID']->image->large          URL to plugin image (large size)
+ * ['plugin_ID']->rating       *        1.0-5.0 plugin rating from wporg
+ * ['plugin_ID']->wporg_url             wporg/extends URL page for this plugin
  *
  * Properties marked with an asterisk are auto-updated periodically from wporg.
  *
@@ -77,9 +79,11 @@ function dpa_get_supported_plugins() {
 		if ( ! $data ) {
 			// Build fallback before querying wporg
 			$plugin               = new stdClass;
+			$plugin->contributors = array();
 			$plugin->name         = $plugin_data['name'];
 			$plugin->description  = $plugin_data['description'];
 			$plugin->image->large = esc_url( $achievements->plugin_url . 'images/' . $slug . '.png' );
+			$plugin->rating       = 0.0;
 			$plugin->wporg_url    = esc_url( 'http://wordpress.org/extend/plugins/' . $slug );
 
 			// Query wporg
@@ -90,19 +94,67 @@ function dpa_get_supported_plugins() {
 
 			// Overwrite the fallback data with new values from wporg
 			if ( ! is_wp_error( $wporg ) ) {
-				$plugin->name        = $wporg->name;
-				$plugin->description = $wporg->short_description;
+				$plugin->contributors = (array) $wporg->contributors;
+				$plugin->name         = $wporg->name;
+				$plugin->description  = $wporg->short_description;
+				$plugin->rating       = floatval( 0.05 * $wporg->rating );
 			}
 
-			$data = $plugin;
+			// Convert wporg contributors data into profiles.wporg URL and Gravatar.
+			$plugin->contributors = dpa_supported_plugins_get_contributor_data( $plugin->contributors );
 
 			// Cache data for 72 hours
 			set_transient( 'dpa_plugin_' . $slug, $plugin, 60 * 60 * 24 * 3 );
+
+			$data = $plugin;
 		}
 
 		$plugins[] = $data;
 	}
 	
 	return apply_filters( 'dpa_get_supported_plugins', $plugins );
+}
+
+/**
+ * Get profiles.wporg URL and Gravatar from a plugin's raw contributors data, from plugins_api().
+ *
+ * @param array $contributors Raw data for a plugin's contributors, from plugins_api()
+ * @return array Consists of nested arrays of [contributor name, profileswp.org URL, Gravatar URL]
+ * @since 3.0
+ * @todo Figure out how to handle 3rd party plugins adding their own support for Achievements.
+ */
+function dpa_supported_plugins_get_contributor_data( $raw_contributors ) {
+	// We don't know email addresses for Gravatars, so use lookup table
+	$people = array(
+		'apeatling'             => 'http://www.gravatar.com/avatar/bb29d699b5cba218c313b61aa82249da?s=24&d=monsterid&r=g',
+		'Blackphantom'          => 'http://www.gravatar.com/avatar/fa62da3fa8b3997be04448e7280dad29?s=24&d=monsterid&r=g',
+		'boonebgorges'          => 'http://www.gravatar.com/avatar/9cf7c4541a582729a5fc7ae484786c0c?s=24&d=monsterid&r=g',
+		'chexee'                => 'http://www.gravatar.com/avatar/0231c6b98cf90defe76bdad0c3c66acf?s=24&d=monsterid&r=g',
+		'cuny-academic-commons' => 'http://www.gravatar.com/avatar/80c3fc801559bbc7111d5e3f56ac6a4c?s=24&d=monsterid&r=g',
+		'DJPaul'                => 'http://www.gravatar.com/avatar/3bc9ab796299d67ce83dceb9554f75df?s=24&d=monsterid&r=g',
+		'garyc40'               => 'http://www.gravatar.com/avatar/aea5ee57d1e882ad17e95c99265784d1?s=24&d=monsterid&r=g',
+		'jeremyboggs'           => 'http://www.gravatar.com/avatar/2a062a10cb94152f4ab3daf569af54c3?s=24&d=monsterid&r=g',
+		'jghazally'             => 'http://www.gravatar.com/avatar/e20e6b90f2f3862025ede5bf90b80a3d?s=24&d=monsterid&r=g',
+		'johnjamesjacoby'       => 'http://www.gravatar.com/avatar/81ec16063d89b162d55efe72165c105f?s=24&d=monsterid&r=g',
+		'matt'                  => 'http://www.gravatar.com/avatar/767fc9c115a1b989744c755db47feb60?s=24&d=monsterid&r=g',
+		'MrMaz'                 => 'http://www.gravatar.com/avatar/a32efc5efefecb3fb1ef1149e23a077c?s=24&d=monsterid&r=g',
+		'mufasa'                => 'http://www.gravatar.com/avatar/5ba89a2ce585864ce73cafa7e79d114c?s=24&d=monsterid&r=g',
+		'mychelle'              => 'http://www.gravatar.com/avatar/da623a80bd7d7ded418c528a689520a3?s=24&d=monsterid&r=g',
+		'slaFFik'               => 'http://www.gravatar.com/avatar/61fb07ede3247b63f19015f200b3eb2c?s=24&d=monsterid&r=g',
+		'sushkov'               => 'http://www.gravatar.com/avatar/39639fde05c65fae440b775989e55006?s=24&d=monsterid&r=g',
+		'valant'                => 'http://www.gravatar.com/avatar/bbbe17bf36f7df6f19c00f28f3614d4c?s=24&d=monsterid&r=g',
+		'valentinas'            => 'http://www.gravatar.com/avatar/f8393ef67ed5144e15890d60eaa2289e?s=24&d=monsterid&r=g',
+	);
+
+	// Filter for 3rd party plugins
+	$people = apply_filters( 'dpa_supported_plugins_get_contributors', $people );
+
+	// Build array of contributors for the current plugin
+	$contributors = array();
+	foreach ( $raw_contributors as $contributor_slug => $contributor_profile ) {
+		$contributors[$contributor_slug] = ! empty( $people[$contributor_slug] ) ? $people[$contributor_slug] : '';
+	}
+
+	return apply_filters( 'dpa_supported_plugins_get_contributor_data', $contributors, $raw_contributors, $people );
 }
 ?>
