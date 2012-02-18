5bb77ef70233eaafe1936a64ff899fb112acd6b8
Paul Gibbs <paul@byotos.com>
2012-02-18 20:08:16 +0000

    Tweak margins and a colour so things look less ugly while I'm working on real styles :)

--- admin/dpa-supported-plugins.php
+++ admin/dpa-supported-plugins.php
@@ -83,7 +83,7 @@ function dpa_supported_plugins_header() {
 				<li><a class="grid <?php if ( 'grid' == $view ) echo 'current'; ?>" title="<?php esc_attr_e( 'Grid view', 'dpa' ); ?>" href="#"></a></li>
 				<li><a class="list <?php if ( 'list' == $view ) echo 'current'; ?>" title="<?php esc_attr_e( 'List view', 'dpa' ); ?>" href="#"></a></li>
 				<li><a class="detail <?php if ( 'detail' == $view ) echo 'current'; ?>" title="<?php esc_attr_e( 'Detail view', 'dpa' ); ?>" href="#"></a></li>
-				<li><p class="label"><?php _e( 'View', 'dpa' ); ?></p></li>
+				<li class="label"><p><?php _e( 'View', 'dpa' ); ?></p></li>
 				<li class="dpa-toolbar-slider <?php if ( 'grid' == $view ) echo 'current'; ?>"><label for="dpa-toolbar-slider"><?php _e( 'Zoom', 'dpa' ); ?></label>
 					<div data-startvalue="<?php echo esc_attr( $zoom ); ?>" id="dpa-toolbar-slider"></div>
 				</li>
