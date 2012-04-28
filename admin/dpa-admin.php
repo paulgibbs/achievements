f548ff82e1926e85f5cb6f895d71082e2e139947
Paul Gibbs <paul@byotos.com>
2012-04-28 13:24:17 +0100

    Rename admin functions.php file for consistency

--- admin/dpa-admin.php
+++ admin/dpa-admin.php
@@ -75,7 +75,7 @@ class DPA_Admin {
 	 * @since 3.0
 	 */
 	private function includes() {
-		require( $this->admin_dir . 'dpa-functions.php' );
+		require( $this->admin_dir . 'dpa-admin-functions.php' );
 		require( $this->admin_dir . 'dpa-supported-plugins.php' );  // Supported plugins screen
 	}
 
