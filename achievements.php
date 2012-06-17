d74fd0251b1eaace4d994011e566cb9d893fc2eb
Paul Gibbs <paul@byotos.com>
2012-06-17 17:45:02 +0100

    Fix problem with the achievements()->file path being built incorrectly.

--- achievements.php
+++ achievements.php
@@ -181,7 +181,7 @@ final class Achievements {
 			$this->file = $GLOBALS['network_plugin'];
 
 		// Paths - plugin
-		$this->file       = WP_PLUGIN_DIR . '/' . $this->file;
+		$this->file       = $this->file;
 		$this->basename   = apply_filters( 'dpa_plugin_basename', 'achievements/achievements.php' );
 		$this->plugin_dir = apply_filters( 'dpa_plugin_dir',      plugin_dir_path( $this->file )  );
 		$this->plugin_url = apply_filters( 'dpa_plugin_url',      plugin_dir_url(  $this->file )  );
--- includes/class-dpa-addon.php
+++ includes/class-dpa-addon.php
@@ -17,7 +17,17 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 interface DPA_Plugin_Interface {
 	/**
-	 * Returns array of key/value pairs for each contributor to this plugin (user name, gravatar URL, profile URL)
+	 * Returns details of actions from this plugin that Achievements can use.
+	 *
+	 * Note that you still have to add these into the dpa_event taxonomy yourself.
+	 *
+	 * @return array e.g. array[0] = array( 'action' => 'publish_post', name => 'Publish blog post', 'description' => 'This event occurs when a blog post has been published. Reward authors for publishing frequently.' );
+	 * @since 3.0
+	 */
+	public function get_actions();
+
+	/**
+	 * Returns array of key/value pairs for each contributor to this plugin (user name, gravatar URL, profile URL).
 	 *
 	 * @return array e.g. array[0] = array( 'username' => 'Paul Gibbs', 'gravatar' => 'http://www.gravatar.com/avatar/3bc9ab796299d67ce83dceb9554f75df', 'profile' => 'http://profiles.wordpress.org/DJPaul' )
 	 * @since 3.0
@@ -79,27 +89,23 @@ interface DPA_Plugin_Interface {
 }
 
 /**
- * Adding support to Achievements for your plugin can be a little a tricky if
- * the action is a WordPress core action for custom post types. This abstract
+ * Adding support to Achievements for your plugin can be a little complicated if
+ * the action is a built-in WordPress action for a post types. This abstract
  * class gives you a starting point to more easily add support for such plugins.
  *
  * @since 3.0
  */
 abstract class DPA_CPT_Plugin implements DPA_Plugin_Interface {
 	/**
-	 * Constructor. Hook into actions/filters that we need to modify.
+	 * For actions that are in WordPress core and handle post types, update the
+	 * user ID from the logged in user to the post author's ID (e.g. for draft
+	 * posts which are then published by another user).
 	 *
-	 * @since 3.0
-	 */
-	public function __construct() {
-		add_filter( 'dpa_handle_event_user_id', array( $this, 'modify_user_id' ), 10, 3 );
-	}
-
-	/**
-	 * For WordPress core actions that are used by custom post types, update the
-	 * user ID from the logged in user to the post author's ID.
+	 * In your class you need to add_filter this method to 'dpa_handle_event_user_id'.
+	 * In your implementation you need to check that $event_name matches the
+	 * name of the action that your plugin implements.
 	 *
-	 * e.g. for draft posts which are then published by another user.
+	 * This method assumes that $func_args[0] is the Post object.
 	 *
 	 * @param int    $user_id    Logged in user's ID
 	 * @param string $event_name Name of the event
@@ -107,8 +113,8 @@ abstract class DPA_CPT_Plugin implements DPA_Plugin_Interface {
 	 * @return int New user ID
 	 * @since 3.0
 	 */
-	public function modify_user_id( $user_id, $event_name, $func_args ) {
-		$post = $action_func_args[0];
+	public function modify_user_id_for_post( $user_id, $event_name, $func_args ) {
+		$post = $func_args[0];
 
 		if ( ! empty( $post->post_author ) )
 			return (int) $post->post_author;
