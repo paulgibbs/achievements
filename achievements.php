2286157d7752f245e6e4dddee2d1301df4969019
Paul Gibbs <paul@byotos.com>
2012-06-29 07:08:54 +0100

    Add WP eCommerce extension to Achievements::includes()

--- achievements.php
+++ achievements.php
@@ -255,7 +255,7 @@ final class Achievements {
 		require( $this->plugin_dir . 'includes/extension-wordpress.php'      ); // Extension for WordPress core
 		require( $this->plugin_dir . 'includes/extension-buddystream.php'    ); // Extension for BuddyStream
 		require( $this->plugin_dir . 'includes/extension-inviteanyone.php'   ); // Extension for Invite Anyone
-
+		require( $this->plugin_dir . 'includes/extension-wpecommerce.php'    ); // Extension for WP e-Commerce
 
 		/**
 		 * Components
