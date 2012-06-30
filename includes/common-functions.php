dcd89aedd04446b2aae970fd9d8eb5d796e9bc4d
Paul Gibbs <paul@byotos.com>
2012-06-30 13:25:02 +0100

    Remove 'dpa_widget' checks.
    Ported from bbPress revision 4033

--- includes/common-functions.php
+++ includes/common-functions.php
@@ -22,23 +22,18 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function dpa_get_paged() {
 	global $wp_query;
 
-	// Make sure to not paginate widget queries
-	if ( ! dpa_is_query_name( 'dpa_widget' ) ) {
-		$paged = 0;
+	// Check the query var
+	if ( get_query_var( 'paged' ) ) {
+		$paged = get_query_var( 'paged' );
 
-		// Check the query var
-		if ( get_query_var( 'paged' ) ) {
-			$paged = get_query_var( 'paged' );
-
-		// Check query paged
-		} elseif ( ! empty( $wp_query->query['paged'] ) ) {
-			$paged = $wp_query->query['paged'];
-		}
-
-		// Paged found
-		if ( ! empty( $paged ) )
-			return (int) $paged;
+	// Check query paged
+	} elseif ( !empty( $wp_query->query['paged'] ) ) {
+		$paged = $wp_query->query['paged'];
 	}
+
+	// Paged found
+	if ( !empty( $paged ) )
+		return (int) $paged;
 
 	// Default to first page
 	return 1;
