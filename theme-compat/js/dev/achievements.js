de20465fe9c39dcbdd82007409d86c9d56b11612
Paul Gibbs <paul@byotos.com>
2012-08-11 21:58:31 +0100

    When the notification closes, redirect to the achievement's permalink.
    * See #27

--- theme-compat/js/dev/achievements.js
+++ theme-compat/js/dev/achievements.js
@@ -5,7 +5,7 @@
  * Creates a notification using jQuery noty.
  *
  * @param string message The text to show in the notification
- * @param string url The link to the achievement page for this notification
+ * @param string url The permalink to the achievement page for this notification
  * @since 3.0
  * @todo Migrate notification to Web Notifications when spec matures
  */
@@ -16,7 +16,14 @@ function dpa_send_notification(message, url) {
 		layout:       'topCenter',
 		dismissQueue: true,
 		text:         message,
-		type:         'success'
+		type:         'success',
+
+		callback: {
+			// When the notification closes, redirect to the achievement's permalink
+			onClose: function() {
+				window.location = url;
+			}
+		}
 	});
 }
 
