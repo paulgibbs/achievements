fc9b5eda708a9a2fd59c33fa9c8af22edfdae52d
Paul Gibbs <paul@byotos.com>
2012-08-14 20:19:52 +0100

    Add new templates for theme compat pagination and no-results screens

---
+++ theme-compat/achievements/feedback-no-achievements.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * No achievements found template part
+ *
+ * @package Achievements
+ * @subpackage ThemeCompatibility
+ */
+?>
+
+<div class="dpa-template-notice">
+	<p><?php _e( 'Oh dear, no achievements were found. Have you considered adopting one?', 'dpa' ); ?></p>
+</div>
---
+++ theme-compat/achievements/pagination-achievements.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Pagination for pages of achievements
+ *
+ * @package Achievements
+ * @subpackage ThemeCompatibility
+ */
+?>
+
+<?php do_action( 'dpa_template_before_pagination_loop' ); ?>
+
+<div class="dpa-pagination">
+	<div class="dpa-pagination-count">
+
+		<?php dpa_achievement_pagination_count(); ?>
+
+	</div>
+
+	<div class="dpa-pagination-links">
+
+		<?php dpa_achievement_pagination_links(); ?>
+
+	</div>
+</div>
+
+<?php do_action( 'dpa_template_after_pagination_loop' ); ?>
