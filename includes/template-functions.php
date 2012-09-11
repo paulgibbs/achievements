8628bec6f853ad061892c857447a6ee7ddf13a6d
Paul Gibbs <paul@byotos.com>
2012-09-11 21:06:13 +0100

    Use theme directory functions instead of constants in dpa_locate_template().
    Ported from bbPress revision 1950

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -41,7 +41,7 @@ function dpa_get_template_part( $slug, $name = null ) {
 /**
  * Retrieve the name of the highest priority template file that exists.
  *
- * Searches in the STYLESHEETPATH before TEMPLATEPATH so that themes which
+ * Searches in the child theme before the parent theme so that themes which
  * inherit from a parent theme can just overload one file. If the template is
  * not found in either of those, it looks in the theme-compat folder last.
  *
@@ -62,21 +62,24 @@ function dpa_locate_template( $template_names, $load = false, $require_once = tr
 			continue;
 
 		// Trim off any slashes from the template name
-		$template_name = ltrim( $template_name, '/' );
+		$template_name  = ltrim( $template_name, '/' );
+		$child_theme    = get_stylesheet_directory(); 
+		$parent_theme   = get_template_directory(); 
+		$fallback_theme = dpa_get_theme_compat_dir(); 
 
 		// Check child theme first
-		if ( file_exists( trailingslashit( STYLESHEETPATH ) . $template_name ) ) {
-			$located = trailingslashit( STYLESHEETPATH ) . $template_name;
+		if ( file_exists( trailingslashit( $child_theme ) . $template_name ) ) {
+			$located = trailingslashit( $child_theme ) . $template_name;
 			break;
 
 		// Check parent theme next
-		} elseif ( file_exists( trailingslashit( TEMPLATEPATH ) . $template_name ) ) {
-			$located = trailingslashit( TEMPLATEPATH ) . $template_name;
+		} elseif ( file_exists( trailingslashit( $parent_theme ) . $template_name ) ) {
+			$located = trailingslashit( $parent_theme ) . $template_name;
 			break;
 
 		// Check theme compatibility last
-		} elseif ( file_exists( trailingslashit( dpa_get_theme_compat_dir() ) . $template_name ) ) {
-			$located = trailingslashit( dpa_get_theme_compat_dir() ) . $template_name;
+		} elseif ( file_exists( trailingslashit( $fallback_theme ) . $template_name ) ) {
+			$located = trailingslashit( $fallback_theme ) . $template_name;
 			break;
 		}
 	}
