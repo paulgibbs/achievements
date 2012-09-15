ee7c31218bdba41a48f2e906d74325eec9d0acd6
Paul Gibbs <paul@byotos.com>
2012-09-15 11:55:48 +0100

    Add functions to build a link to a user's profile.

--- includes/user-functions.php
+++ includes/user-functions.php
@@ -159,6 +159,11 @@ function dpa_is_user_deleted( $user_id = 0 ) {
 	return apply_filters( 'dpa_is_user_deleted', (bool) $is_deleted );
 }
 
+
+/**
+ * These things happen when a user unlocks an achievement.
+ */
+
 /**
  * When an achievement is unlocked, give the points to the user.
  *
@@ -182,3 +187,58 @@ function dpa_send_points( $achievement_obj, $user_id, $progress_id ) {
 	// Allow other things to happen after the user's points have been updated
 	do_action( 'dpa_send_points', $achievement_obj, $user_id, $progress_id, $points );
 }
+
+
+/**
+ * User avatar
+ */
+
+/**
+ * Output the avatar link of a user
+ *
+ * @param array $args See dpa_get_user_avatar_link() documentation.
+ * @since 3.0
+ */
+function dpa_user_avatar_link( $args = 0 ) {
+	echo dpa_get_user_avatar_link( $args );
+}
+	/**
+	 * Return the avatar link of a user
+	 *
+	 * @param array $args This function supports these arguments:
+	 *  - int $size If we're showing an avatar, set it to this size
+	 *  - string $type What type of link to return; either "avatar", "name", or "both".
+	 *  - int $user_id The ID for the user.
+	 * @return string
+	 * @since 3.0
+	 */
+	function dpa_get_user_avatar_link( $args = array() ) {
+		$defaults = array(
+			'size'    => 50,
+			'type'    => 'both',
+			'user_id' => 0,
+		);
+		$r = dpa_parse_args( $args, $defaults, 'get_user_avatar_link' );
+		extract( $r );
+
+		// Default to current user
+		if ( empty( $user_id ) && is_user_logged_in() )
+			$user_id = get_current_user_id();
+
+		// Assemble some link bits
+		$user_link = array();
+		$user_url  = get_author_posts_url( $user_id );
+
+		// Get avatar
+		if ( 'avatar' == $type || 'both' == $type )
+			$user_link[] = sprintf( '<a href="%1$s">%2$s</a>', esc_url( $user_url ), get_avatar( $user_id, $size ) );
+
+		// Get display name
+		if ( 'avatar' != $type )
+			$user_link[] = sprintf( '<a href="%1$s">%2$s</a>', esc_url( $user_url ), get_the_author_meta( 'display_name', $user_id ) );
+
+		// Piece together the link parts and return
+		$user_link = join( '&nbsp;', $user_link );
+
+		return apply_filters( 'dpa_get_user_avatar_link', $user_link, $args );
+	}
