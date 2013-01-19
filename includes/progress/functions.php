3914c934ce179ed021dccbbf4dd7039da3a8ba07
Paul Gibbs <paul@byotos.com>
2013-01-19 10:14:24 +0000

    Add dpa_delete_achievement_progress()
    Delete a user's progress for an achievement. Essentially, un-reward the
    achievement for this user.

--- includes/progress/functions.php
+++ includes/progress/functions.php
@@ -47,3 +47,34 @@ function dpa_get_locked_status_id() {
 function dpa_get_unlocked_status_id() {
 	return apply_filters( 'dpa_get_unlocked_status_id', achievements()->unlocked_status_id );
 }
+
+/**
+ * Delete a user's progress for an achievement. Essentially, un-reward the achievement for this user.
+ *
+ * @param int $achievement_id Achievement ID
+ * @param int $user_id User ID
+ * @since Achievements (3.0)
+ */
+function dpa_delete_achievement_progress( $achievement_id, $user_id ) {
+	$achievement_id = dpa_get_achievement_id( $achievement_id );
+	if ( empty( $achievement_id ) || ! dpa_is_achievement( $achievement_id ) )
+		return;
+
+	do_action( 'dpa_before_delete_achievement_progress', $achievement_id, $user_id, $progress_id  );
+
+	dpa_has_progress( array(
+		'ach_populate_achievements' => true,
+		'author'                    => $user_id,
+		'posts_per_page'            => -1,
+	) );
+
+	// Look in the progress posts and match against a post_parent that is the same as the desired achievement
+	$progress = wp_filter_object_list( achievements()->progress_query->posts, array( 'post_parent' => $achievement_id ) );
+	$progress = array_shift( $progress );
+
+	if ( empty( $progress ) )
+		return;
+
+	$progress_id = apply_filters( 'dpa_delete_achievement_progress', $progress->ID, $achievement_id, $user_id );
+	wp_delete_post( $progress_id, true );
+}
