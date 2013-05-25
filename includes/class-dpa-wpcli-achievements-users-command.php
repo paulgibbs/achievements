da580f9e94607262de2a7813ddd3bb158cebec84
Paul Gibbs <paul@byotos.com>
2013-05-25 15:47:04 +0100

    Add new WP_CLI command to award an achievement to a user

--- includes/class-dpa-wpcli-achievements-users-command.php
+++ includes/class-dpa-wpcli-achievements-users-command.php
@@ -58,6 +58,7 @@ class DPA_WPCLI_Achievements_Users_Command extends WP_CLI_Command {
 	/**
 	 * Remove an unlocked achievement from a user
 	 *
+	 * @alias remove
 	 * @since Achievements (3.4)
 	 * @synopsis --user_id=<id> --achievement=<postname>
 	 */
@@ -71,15 +72,64 @@ class DPA_WPCLI_Achievements_Users_Command extends WP_CLI_Command {
 		if ( ! $achievement_id )
 			WP_CLI::error( sprintf( 'Achievement ID not found for post_name: %1$s', $achievement_id ) );
 
+		// Has the user already been awarded this achievement?
 		if ( dpa_has_user_unlocked_achievement( $assoc_args['user_id'], $achievement_id ) ) {
 			dpa_delete_achievement_progress( $achievement_id, $assoc_args['user_id'] );
 
 			WP_CLI::success( sprintf( 'Achievement ID %1$s has been revoked from User ID %2$s', $achievement_id, $assoc_args['user_id'] ) );
 		} else {
 			WP_CLI::warning( sprintf( 'User ID %1$s has not unlocked achievement ID %2$s', $assoc_args['user_id'], $achievement_id ) );
+			return;
 		}
 	}
 
+	/**
+	 * Award an achievement to a user
+	 *
+	 * @alias add
+	 * @since Achievements (3.4)
+	 * @synopsis --user_id=<id> --achievement=<postname>
+	 */
+	public function award( $args, $assoc_args ) {
+
+		if ( ! $assoc_args['user_id'] || ! $user = get_userdata( $assoc_args['user_id'] ) )
+			WP_CLI::error( 'Invalid User ID specified.' );
+
+		// Get the achievement ID
+		$achievement_id = $this->_get_achievement_id_by_post_name( $assoc_args['achievement'] );
+		if ( ! $achievement_id )
+			WP_CLI::error( sprintf( 'Achievement ID not found for post_name: %1$s', $achievement_id ) );
+
+		// If the user has already unlocked this achievement, bail out.
+		if ( dpa_has_user_unlocked_achievement( $assoc_args['user_id'], $achievement_id ) ) {
+			WP_CLI::warning( sprintf( 'User ID %1$s has already unlocked achievement ID %2$s', $assoc_args['user_id'], $achievement_id ) );
+			return;
+		}
+
+		$achievement_obj = dpa_get_achievements( array(
+			'no_found_rows' => true,
+			'nopaging'      => true,
+			'numberposts'   => 1,
+			'p'             => $achievement_id,
+		) );
+		$achievement_obj = $achievement_obj[0];
+
+		// Find any still-locked progress for this achievement for this user, as dpa_maybe_unlock_achievement() needs it.
+		$progress_obj = dpa_get_progress( array(
+			'author'        => $assoc_args['user_id'],
+			'no_found_rows' => true,
+			'nopaging'      => true,
+			'numberposts'   => 1,
+			'post_status'   => dpa_get_locked_status_id(),
+		) );
+
+		if ( empty( $progress_obj ) )
+			$progress_obj = array();
+
+		// Award the achievement
+		dpa_maybe_unlock_achievement( $assoc_args['user_id'], 'skip_validation', $progress_obj, $achievement_obj );
+		WP_CLI::success( sprintf( 'Achievement ID %1$s has been awarded to User ID %2$s', $achievement_id, $assoc_args['user_id'] ) );
+	}
 
 	/**
 	 * Helper methods for the CLI commands to avoid code duplication
