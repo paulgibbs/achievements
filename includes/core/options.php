08ecd6b14bf6e203a8460f171b194ae6be32453f
Paul Gibbs <paul@byotos.com>
2013-07-28 17:34:42 +0100

    Add first pass of leaderboard function to users component

--- includes/core/options.php
+++ includes/core/options.php
@@ -179,6 +179,22 @@ function dpa_get_progresses_per_rss_page() {
 	return (int) apply_filters( 'dpa_get_progresses_per_rss_page', $retval, $per );
 }
 
+/**
+ * Return the leaderboard items per page setting
+ *
+ * @return int
+ * @since Achievements (3.4)
+ */
+function dpa_get_leaderboard_items_per_page() {
+	$default = 12;
+	$per     = $retval = (int) get_option( '_dpa_leaderboard_per_page', $default );
+
+	if ( empty( $retval ) )
+		$retval = $default;
+
+	return (int) apply_filters( 'dpa_get_leaderboard_items_per_page', $retval, $per );
+}
+
 
 /**
  * Boolean functions (aka is this thing on?)
--- includes/users/functions.php
+++ includes/users/functions.php
@@ -79,3 +79,93 @@ function dpa_get_displayed_user_id() {
 
 	return apply_filters( 'dpa_get_displayed_user_id', $retval );
 }
+
+/**
+ * Get the current state of the leaderboard, sorted by users' karma points.
+ *
+ * @param array $args Optional. Associatve array of optional arguments.
+ * @since Achievements (3.4)
+ */
+function dpa_get_leaderboard( array $args = array() ) {
+	global $wpdb;
+
+	$defaults = array(
+		'paged'          => dpa_get_paged(),                       // Page number
+		'posts_per_page' => dpa_get_leaderboard_items_per_page(),  // Users per page
+		'user_id'        => 0,                                     // Get details for a specific user if non-zero; pass an array of ints for >1 user.
+	);
+
+	$args = dpa_parse_args( $args, $defaults, 'get_leaderboard' );
+
+	// No, we're not allowing infinite results. This is always a bad idea.
+	if ( (int) $args['posts_per_page'] < 1 )
+		$args['posts_per_page'] = dpa_get_leaderboard_items_per_page();
+
+	$points_key = "{$wpdb->prefix}_dpa_points";
+	$num_users  = ( $args['user_id'] !== 0 ) ? count( (array) $args['user_id'] ) : 0;
+
+
+	/**
+	 * Build the SQL
+	 */
+
+	// 1) Get all meta_values for everyone's "_dpa_points" keys as integers, and order descending.
+	$subquery = $wpdb->prepare(
+		"SELECT GROUP_CONCAT(DISTINCT ranking.meta_value ORDER BY CONVERT(ranking.meta_value, signed) DESC)
+		FROM {$wpdb->usermeta} AS ranking
+		WHERE ranking.meta_key = %s",
+	$points_key );
+
+	// I would add "LIMIT {$num_users}", but the MySQL docs say: "if LIMIT occurs within a subquery and also is applied in the outer query, the outermost LIMIT takes precedence".
+	if ( $num_users > 0 )
+		$subquery .= ' AND ranking.user_id IN (' . implode( ',', wp_parse_id_list( (array) $args['user_id'] ) ) . ')';
+
+
+	// 2) Get each user's rank, user ID, and meta_value.
+	$query = $wpdb->prepare(
+		"SELECT SQL_CALC_FOUND_ROWS FIND_IN_SET( karma.meta_value, ({$subquery}) ) as rank, ID, karma.meta_value
+		FROM {$wpdb->users} AS person
+		LEFT JOIN {$wpdb->usermeta} as karma ON person.id = karma.user_id AND karma.meta_key = %s",
+	$points_key );
+
+	/**
+	 * Sort users correctly even if some of them don't have any karma points.
+	 * `ORDER BY... rank` causes a table sort because usermeta has no index on meta_value, so only add this if we need it.
+	 */
+	if ( $num_users !== 1 )
+		$query .= " ORDER BY CASE WHEN rank IS NULL THEN 1 ELSE 0 END, rank";
+
+	// Handle pagination
+	$offset  = ( (int) $args['paged'] - 1 ) * (int) $args['posts_per_page'];
+	$query  .= $wpdb->prepare( ' LIMIT %d, %d', $offset, $args['posts_per_page'] );
+
+
+	/**
+	 * See if we already have this query in cache
+	 */
+
+	$last_changed = wp_cache_get( 'last_changed', 'achievements_leaderboard' );
+	if ( ! $last_changed ) {
+		$last_changed = microtime();
+		wp_cache_set( 'last_changed', $last_changed, 'achievements_leaderboard' );
+	}
+
+	$cache_key = 'get_leaderboard:' . md5( serialize( $query ) ) . ":$last_changed";
+	if ( $cache = wp_cache_get( $cache_key, 'achievements_leaderboard' ) )
+		return $cache;
+	// djpaultodo: what are we storing in cache here? what do we need to do to hte data?
+
+
+	/**
+	 * Run the query and cache results
+	 */
+	$results       = $wpdb->get_results( $query );
+	$results_found = $wpdb->get_var( 'SELECT FOUND_ROWS()' );
+
+	$results = array(
+		'results' => $results,
+		'total'   => (int) $results_found,
+	);
+
+	wp_cache_set( $cache_key, $results, 'achievements_leaderboard' );
+}
