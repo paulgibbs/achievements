56218bb0d494db4056372d5392ea8fe05b754181
Paul Gibbs <paul@byotos.com>
2013-09-25 11:37:33 +0100

    Bump version number in to latest release version - must have been messed up in a previous merge/commit

--- src/achievements.php
+++ src/achievements.php
@@ -13,7 +13,7 @@
 Plugin Name: Achievements
 Plugin URI: http://achievementsapp.com/
 Description: Achievements gamifies your WordPress site with challenges, badges, and points.
-Version: 3.4
+Version: 3.4.1
 Requires at least: 3.6.1
 Tested up to: 3.6.20
 License: GPLv3
