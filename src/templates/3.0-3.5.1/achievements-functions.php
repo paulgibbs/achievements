4ac0b05340de5ba3dcd4ccafc5e48c326b9bdc3a
Paul Gibbs <paul@byotos.com>
2014-04-13 16:41:00 +0100

    Move old notifications files into 3.5.x theme pack
    The new 3.6/default won’t support backwards compatibility for the old,
    no heartbeat, notifications that were deprecated in 3.5.1. Users will
    have to switch to the 3.0-3.5.1 theme pack if they want to continue to
    use them.
    
    This commit re-implements this backwards compatibility change to remove
    the junk out of the core files.

--- src/templates/3.0-3.5.1/achievements-functions.php
+++ src/templates/3.0-3.5.1/achievements-functions.php
@@ -62,6 +62,12 @@ class DPA_Default extends DPA_Theme_Compat {
 		add_action( 'dpa_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		add_action( 'dpa_footer',          array( $this, 'print_notification_templates' ) );
 
+		// Deprecated notifications functionality (3.5)
+		if ( dpa_deprecated_notification_template_exists() ) {
+			add_action( 'dpa_enqueue_scripts', 'dpa_deprecated_enqueue_notification_styles_and_scripts' );
+			add_action( 'dpa_footer',          'dpa_deprecated_print_notifications'                     );
+		}
+
 		do_action_ref_array( 'dpa_theme_compat_actions', array( &$this ) );
 	}
 
@@ -234,3 +240,118 @@ class DPA_Default extends DPA_Theme_Compat {
 	}
 }  // class_exists
 endif;
+
+/**
+ * Does the old-style notification template file exist in the current theme?
+ *
+ * The template was removed in version 3.5 and was replaced with the heartbeat-powered "live notifications" system.
+ * This function has no filters on purpose because it's meant to only run once, as quickly as possible.
+ *
+ * @return bool
+ * @since Achievements (3.5)
+ */
+function dpa_deprecated_notification_template_exists() {
+	static $retval = null;
+
+	if ( $retval !== null )
+		return $retval;
+
+	$template_locations = dpa_add_template_locations( array( '/feedback-achievement-unlocked.php' ) );
+	$retval             = false;
+
+	foreach ( $template_locations as $template_name ) {
+
+		// Try to find the deprecated template. Check child theme first.
+		if ( file_exists( get_stylesheet_directory() . "/$template_name" ) ) {
+			$retval = true;
+			break;
+
+		// Check parent theme last.
+		} elseif ( file_exists( get_template_directory() . "/$template_name" ) ) {
+			$retval = true;
+			break;
+		}
+	}
+
+	return $retval;
+}
+
+/**
+ * Backwards compatibility with pre-3.5; enqueue the old-style CSS/JS for notifications.
+ *
+ * These scripts were removed from the default theme compatiility pack in version 3.5 with
+ * the introduction of the heartbeat-powered "live notifications" system.
+ *
+ * @since Achievements (3.5)
+ */
+function dpa_deprecated_enqueue_notification_styles_and_scripts() {
+
+	// If user's not active or is inside the WordPress Admin, bail out.
+	if ( ! dpa_is_user_active() || is_admin() || is_404() )
+		return;
+
+	/**
+	 * CSS
+	 */
+	$rtl  = is_rtl() ? '-rtl' : '';
+	$file = "css/notifications{$rtl}.css";
+
+	// Check child theme
+	if ( file_exists( trailingslashit( get_stylesheet_directory() ) . $file ) ) {
+		$location = trailingslashit( get_stylesheet_directory_uri() );
+
+	// Check parent theme
+	} elseif ( file_exists( trailingslashit( get_template_directory() ) . $file ) ) {
+		$location = trailingslashit( get_template_directory_uri() );
+
+	// Achievements theme compatibility
+	} else {
+		$location = trailingslashit( dpa_get_theme_compat_url() );
+	}
+
+	wp_enqueue_style( 'dpa-default-notifications', $location . $file, array(), dpa_get_theme_compat_version(), 'screen' );
+
+
+	/**
+	 * JS
+	 */
+	$file = "js/notifications.js";
+
+	// Check child theme
+	if ( file_exists( trailingslashit( get_stylesheet_directory() ) . $file ) ) {
+		$location = trailingslashit( get_stylesheet_directory_uri() );
+
+	// Check parent theme
+	} elseif ( file_exists( trailingslashit( get_template_directory() ) . $file ) ) {
+		$location = trailingslashit( get_template_directory_uri() );
+
+	// Achievements theme compatibility
+	} else {
+		$location = trailingslashit( dpa_get_theme_compat_url() );
+	}
+
+	wp_enqueue_style( 'dpa-default-notifications-javascript', $location . $file, array( 'jquery' ), dpa_get_theme_compat_version(), true );
+}
+
+/**
+ * Backwards compatibility with pre-3.5: print the old-style notifications for the current user to the page footer.
+ * 
+ * Notifications were overhauled in version 3.5 and were replaced with the heartbeat-powered "live notifications" system.
+ * This function used to be called "dpa_print_notifications".
+ *
+ * @deprecated Achievements (3.5)
+ * @since Achievements (3.0)
+ */
+function dpa_deprecated_print_notifications() {
+
+	// If user's not active or is inside the WordPress Admin, bail out.
+	if ( ! dpa_is_user_active() || is_admin() || is_404() || ! dpa_user_has_notifications() )
+		return;
+
+	// Get current notifications
+	$notifications = dpa_get_user_notifications();
+	if ( empty( $notifications ) )
+		return;
+
+	echo achievements()->shortcodes->display_feedback_achievement_unlocked();
+}
